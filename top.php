8540cb49fd59e5115cda47ca15b07d9f236192f8
Givaldo Fernandes <122561742+Gilferprog@users.noreply.github.com>
2023-04-27 11:14:09 -0300

    Mascaras e cor
    Inserido mascaras cpf e celular e cor de fundo.

---
+++ top.php
@@ -0,0 +1,16 @@
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.mask.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <script>
+        $("#cpf").mask("000.000.000.-00");
+		$("#celular").mask("(00)00000-0000");
+	</script>
+	<style type="text/css">
+		body{
+			background-color: #3399FF;
+		}
+	</style>
+

08c62a0f106632de04b58b9a6782d863695af49e
Givaldo Fernandes <122561742+Gilferprog@users.noreply.github.com>
2023-04-27 16:10:52 -0300

    Criado index
    Criei arquivo index e icones del e edt

--- top.php
+++ top.php
@@ -4,6 +4,8 @@
     <script src="js/jquery.mask.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/all.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<!-- Option 1: Include in HTML -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <script>
         $("#cpf").mask("000.000.000.-00");
 		$("#celular").mask("(00)00000-0000");
